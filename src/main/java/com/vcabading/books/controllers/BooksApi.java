72091c9e5d7d377fd025ef120dfa0b107bfb4479
Virgilio Cabading <virgilio.d.cabading.jr@gmail.com>
2021-12-06 23:58:12 -0800

    add BooksApi constructor

--- src/main/java/com/vcabading/books/controllers/BooksApi.java
+++ src/main/java/com/vcabading/books/controllers/BooksApi.java
@@ -1,15 +1,26 @@
 package com.vcabading.books.controllers;
 
+import org.springframework.web.bind.annotation.RestController;
+
 import com.vcabading.books.service.BookService;
 
 /////////////////////////////////////////////////////////////
 // BOOKS API
 /////////////////////////////////////////////////////////////
 
+@RestController
 public class BooksApi {
 	
 	//	//// FIELDS /////////////////////////////////////////
 	
 	private final BookService bookService;
+
+	//	//// CONSTRUCTORS ///////////////////////////////////
+	
+	public BooksApi(BookService bookService) {
+		this.bookService = bookService;
+	}
+	
+	
 	
 }
