20fbd7c6c0f4b218433fe6de71d0f40764b930cc
Virgilio Cabading <virgilio.d.cabading.jr@gmail.com>
2021-12-07 00:05:13 -0800

    add post to api/books

--- src/main/java/com/vcabading/books/controllers/BooksApi.java
+++ src/main/java/com/vcabading/books/controllers/BooksApi.java
@@ -1,7 +1,13 @@
 package com.vcabading.books.controllers;
 
+import java.util.List;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.vcabading.books.models.Book;
 import com.vcabading.books.service.BookService;
 
 /////////////////////////////////////////////////////////////
@@ -22,6 +28,12 @@ public class BooksApi {
 	}
 	
 	//	//// POST ///////////////////////////////////////////
+	
+	@RequestMapping(value="/api/books", method=RequestMethod.POST)
+    public Book create(@RequestParam(value="title") String title, @RequestParam(value="description") String desc, @RequestParam(value="language") String lang, @RequestParam(value="pages") Integer numOfPages) {
+        Book book = new Book(title, desc, lang, numOfPages);
+        return bookService.createBook(book);
+    }
 	
 	//	//// RETRIEVE ///////////////////////////////////////
 	
